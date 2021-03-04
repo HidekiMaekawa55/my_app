7d9da666e3bf53e61dfea4608fcc6786849ab798
HidekiMaekawa55 <koronhideki@icloud.com>
2021-03-04 17:15:58 +0900

    create Users controller new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/profile', to: 'static_pages#profile'
+  get  '/signup',  to: 'users#new'
 end
