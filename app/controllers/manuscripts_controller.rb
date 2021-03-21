f9a1617cd4025c92da35c74c07b4066850c24770
HidekiMaekawa55 <koronhideki@icloud.com>
2021-03-21 23:24:25 +0900

    heroku error

--- app/controllers/manuscripts_controller.rb
+++ app/controllers/manuscripts_controller.rb
@@ -2,7 +2,7 @@ class ManuscriptsController < ApplicationController
   before_action :admin_user, only: [:new, :create, :edit, :destroy]
   
   def index
-    @manuscripts = Manuscript.all.order("created_at DESC")
+    @manuscripts = Manuscript.all
   end
   
   def show
