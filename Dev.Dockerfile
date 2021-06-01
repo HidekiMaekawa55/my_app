1ecdab815c0771e3d18db74117c966b82a957213
HidekiMaekawa55 <koronhideki@icloud.com>
2021-06-01 23:52:22 +0900

    Add chromium-driver to Dockerfile and rspec settings to spec_helper.rb

--- Dev.Dockerfile
+++ Dev.Dockerfile
@@ -2,7 +2,7 @@ FROM ruby:2.6.3
 
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
     && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.lis \
-    && apt-get update -qq && apt-get install -y build-essential nodejs npm yarn sqlite3 libsqlite3-dev 
+    && apt-get update -qq && apt-get install -y build-essential nodejs npm yarn sqlite3 libsqlite3-dev chromium-driver 
 
 WORKDIR /my_app
 COPY Gemfile ./Gemfile
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,17 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   
+  Capybara.register_driver :headless_chrome do |app|
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.args << "--headless"
+      opts.args << "--disable-gpu"
+      opts.args << "--no-sandbox"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  end
+  
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    driven_by :headless_chrome
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
